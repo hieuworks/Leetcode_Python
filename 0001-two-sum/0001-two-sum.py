545c13a4701a309034ea032bd09271d27bceb8af
ĐINH XUÂN HIẾU <dinhxuanhieuct@gmail.com>
2026-01-10 11:30:42 +0700

    Time: 1 ms (57.33%), Space: 20.6 MB (5.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(nums) - 1
-        sum = None
-        while sum!=target and i < len(nums) - 1:
-            for k in range(0,j-i):
-                sum = nums[i] + nums[j]
-                if sum != target:
-                    j-=1
-                elif sum == target:
-                    return[i, j]
-            i+=1
-            j = len(nums) - 1
+        da_gap = {}
+        for j in range(len(nums)):
+            start = nums[j]
+            so_can_tim = target - start
+            if so_can_tim in da_gap:
+                return [da_gap[so_can_tim], j]
+            da_gap[start] = j
         return []
 
