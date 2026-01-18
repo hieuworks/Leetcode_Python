e897c9d4472ba6dedc26ae8947c01a08683312ee
DINH XUAN HIEU <dinhxuanhieuct@gmail.com>
2026-01-18 23:46:01 +0700

    Time: 0 ms (100%), Space: 19.4 MB (15.9%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         p1 = m - 1
         p2 = n - 1
         p = m + n - 1
+        
         while p2 >= 0:
             if p1 >= 0 and nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
