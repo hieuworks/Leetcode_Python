90c59993f27c7ceaa215805902f6c90161120828
DINH XUAN HIEU <dinhxuanhieuct@gmail.com>
2026-01-25 22:44:11 +0700

    Time: 9233 ms (5%), Space: 21.5 MB (10.17%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for x in nums:
-            res ^= x
-        return res
+        if len(nums) == 1:
+            return nums[0]
+        passed_nums = []
+        for i in range(len(nums)):
+            if nums[i] in passed_nums:
+                continue
+            else:
+                for j in range(len(nums)):
+                    if nums[i] == nums[j] and i != j:
+                        passed_nums.append(nums[i])
+                        break
+        for num in nums:
+            if num not in passed_nums:
+                return num
 
+            
